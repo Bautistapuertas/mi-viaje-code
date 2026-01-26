e852028e26ee1ac8219a2fca282ffeaafb1ce3c2
Bautista <puertasbauti7@gmail.com>
2026-01-26 19:11:01 -0300

    ahora si, frase completa

--- conceptos.js
+++ conceptos.js
@@ -1,43 +1,15 @@
-// 1. CAMBIO DE MENTALIDAD:
-// Declara una variable llamada 'nombre' usando CONST con tu nombre:
-
-
-// Declara una variable llamada 'edad' usando LET con tu edad:
-let edad=21
-
-// Intenta cambiar la edad (simula que cumpliste años):
-edad=22
-
-
-// 2. FUNCIÓN FLECHA (ARROW FUNCTION):
-// Transforma esta función antigua a una función flecha moderna:
-/*
-function saludar(nombre) {
-    return "Hola " + nombre + ", listo para aprender React!";
-}
-*/const saludar=nombre=>`Hola ${nombre},listo pata aprender React!`
-
-// Tu versión moderna aquí (usa const y =>):
-// ...
-
-
-// 3. IMPRIMIR RESULTADOS
-console.log("Nombre:", nombre);
-console.log("Edad:", edad);
-// Llama a tu función saludo aquí pasando tu variable 'nombre'
-// console.log( ... );
-console.log(saludar(nombre));
-
-const usuario={
-    nombre:"Bautista",
-    stack:["Git","javaScript","React"],
+// 1. Definimos el objeto con tus datos
+const usuario = {
+    nombre: "Bautista",
+    stack: ["Git", "React", "JS"],
     disponible: true
 };
-// 2. Destructuring (Extracción)
-// OJO: Usamos 'disponible' porque así se llama en el objeto de arriba.
+
+// 2. Destructuring (Sacamos los datos fuera del objeto)
 const { nombre, stack, disponible } = usuario;
 
-// Comprobación (opcional)
-console.log(nombre); // Imprime: Bautista
-const frase=`hola, soy ${nombre}, y domino ${stack[0]},${stack[1]}`;
-console.log(frase)
+// 3. Template Strings (La frase final con backticks)
+const frase = `Hola, soy ${nombre} y domino ${stack[0]} y ${stack[1]}`;
+
+// 4. Imprimimos el resultado
+console.log(frase);
